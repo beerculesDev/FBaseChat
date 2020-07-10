a17533441890489db429609f1982d6b65f459203
beerculesDev <11017852@cc.peralta.edu>
2020-07-09 21:22:19 -0700

    Initial Commit

---
+++ FBchatTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FBchatTutorial
+//
+//  Created by Jasson and Meggie on 7/9/20.
+//  Copyright © 2020 Jasson and Meggie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
